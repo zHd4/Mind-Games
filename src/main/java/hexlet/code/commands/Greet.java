856ba6f0f50fef313be16e3cbb0661de2f453031
zHd4 <computersystembase@gmail.com>
2022-05-31 23:38:59 +0200

    Created getUserName() and greeting() in hexlet.code.commands.Greet

--- src/main/java/hexlet/code/commands/Greet.java
+++ src/main/java/hexlet/code/commands/Greet.java
@@ -3,19 +3,27 @@ package hexlet.code.commands;
 import hexlet.code.Cli;
 
 public final class Greet implements Command {
+    private static String userName = null;
+
+    public static String getUserName() {
+        return userName;
+    }
 
     @Override
     public void execute() {
-        System.out.println("Welcome to the Brain Games!");
-
-        String userName = Cli.getUserName();
-
-        System.out.printf("Hello, %s!\n", userName);
-        System.exit(0); //
+        greeting();
+        System.exit(0);
     }
 
     @Override
     public int getCommandNumber() {
         return 1;
+    }
+
+    public void greeting() {
+        System.out.println("Welcome to the Brain Games!");
+
+        userName = Cli.getUserName();
+        System.out.printf("Hello, %s!\n", userName);
     }
 }

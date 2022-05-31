5987a662b4d85502cf06bc2e0bcbc2a108b9641c
zHd4 <computersystembase@gmail.com>
2022-05-31 23:37:28 +0200

    Created inputYesNo() in hexlet.code.Cli

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -4,14 +4,15 @@ import hexlet.code.exceptions.InvalidMenuNumberChoiceException;
 import java.util.Scanner;
 
 public class Cli {
+    private static final Scanner USER_INPUT_SCANNER = new Scanner(System.in);
+
     public static String getUserName() {
         String userName = null;
 
         while (userName == null) {
             System.out.print("May I have your name? ");
 
-            Scanner userInputScanner = new Scanner(System.in);
-            String inputResult = userInputScanner.nextLine();
+            String inputResult = USER_INPUT_SCANNER.nextLine();
 
             if (inputResult.length() > 0) {
                 userName = inputResult;
@@ -24,19 +25,24 @@ public class Cli {
     public static Integer getUserChoice() throws InvalidMenuNumberChoiceException {
         System.out.print("Your choice: ");
 
-        Scanner userInputScanner = new Scanner(System.in);
-        String inputResult = userInputScanner.nextLine();
+        String inputResult = USER_INPUT_SCANNER.nextLine();
 
         try {
             Integer result = Integer.parseInt(inputResult);
 
             System.out.print('\n');
-
             return result;
         } catch (NumberFormatException e) {
             System.out.println("Enter a valid number!\n");
             throw new InvalidMenuNumberChoiceException();
         }
+    }
+
+    public static boolean inputYesNo(String text) {
+        System.out.print(text);
+
+        String inputResult = USER_INPUT_SCANNER.nextLine().toLowerCase();
+        return inputResult.equals("yes");
     }
 }
 

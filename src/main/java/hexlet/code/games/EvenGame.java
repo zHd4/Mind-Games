416eb9412be0ccb16ad4a81135e233b2b7b516c3
zHd4 <computersystembase@gmail.com>
2022-05-31 23:43:44 +0200

    Created and implemented hexlet.code.games.EvenGame

---
+++ src/main/java/hexlet/code/games/EvenGame.java
@@ -0,0 +1,61 @@
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.commands.Greet;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class EvenGame implements Game {
+    private static final int RANDOM_MIN = 22;
+
+    private static final int RANDOM_MAX = 3333;
+
+    private static final int MAX_CORRECT_ANSWERS = 3;
+
+    private boolean gameRunning = false;
+
+    @Override
+    public void startGameLoop() {
+        int correctAnswersCount = 0;
+
+        switchGameState();
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        while (gameRunning) {
+            int question = randomInt();
+            boolean isEven = question % 2 == 0;
+
+            System.out.printf("Question: %s\n", question);
+
+            boolean answer = Cli.inputYesNo("Your answer: ");
+
+            if (answer == isEven) {
+                correctAnswersCount++;
+
+                System.out.println("Correct!\n");
+
+                if (correctAnswersCount == MAX_CORRECT_ANSWERS) {
+                    System.out.printf("Congratulations, %s!\n\n", Greet.getUserName());
+                    switchGameState();
+                }
+            } else {
+                String userAnswerString = answer ? "yes" : "no";
+                String correctAnswerString = isEven ? "yes" : "no";
+
+                System.out.printf("'%s' is wrong answer ;(. Correct answer was '%s'.\n",
+                        userAnswerString, correctAnswerString);
+
+                System.out.printf("Let's try again, %s!\n\n", Greet.getUserName());
+                switchGameState();
+            }
+        }
+    }
+
+    private int randomInt() {
+        return ThreadLocalRandom.current().nextInt(RANDOM_MIN, RANDOM_MAX + 1);
+    }
+
+    private void switchGameState() {
+        gameRunning = !gameRunning;
+    }
+}

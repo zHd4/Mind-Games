d0f30d79be45ab4f3510ad25b377ebd2bc26a192
zHd4 <computersystembase@gmail.com>
2023-12-24 15:47:01 +0100

    Update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,19 +1,24 @@
 package hexlet.code;
 
-import hexlet.code.games.EvenGame;
-import hexlet.code.games.Game;
+import hexlet.code.games.*;
 
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class App {
-    private static final Map<Integer, Game> GAMES_MAP = Map.of(2, new EvenGame());
+    private static final Map<Integer, Game> GAMES_MAP = Map.of(
+            2, new EvenGame(),
+            3, new CalculatorGame(),
+            4, new GCDGame(),
+            5, new ProgressionGame(),
+            6, new PrimeGame());
 
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
 
-        GAMES_MAP.forEach((number, game) -> System.out.printf("%o - %s\n", number, game));
+        new TreeMap<>(GAMES_MAP).forEach((number, game) -> System.out.printf("%s - %s\n", number, game));
 
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");

7a609fd1ca3a27ee8e4a3393a9f3a033633cf027
zHd4 <computersystembase@gmail.com>
2023-12-26 15:59:42 +0100

    Update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,32 +1,27 @@
 package hexlet.code;
 
 
-import hexlet.code.games.Game;
 import hexlet.code.games.EvenGame;
 import hexlet.code.games.CalculatorGame;
 import hexlet.code.games.GCDGame;
 import hexlet.code.games.ProgressionGame;
 import hexlet.code.games.PrimeGame;
 
-import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
 public final class App {
-    private static final Map<Integer, Game> GAMES_MAP = Map.of(
-            2, new EvenGame(),
-            3, new CalculatorGame(),
-            4, new GCDGame(),
-            5, new ProgressionGame(),
-            6, new PrimeGame());
-
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet");
+        System.out.println("""
+                           Please enter the game number and press Enter.
+                           1 - Greet
+                           2 - Even
+                           3 - Calc
+                           4 - GCD
+                           5 - Progression
+                           6 - Prime
+                           0 - Exit
+                           """);
 
-        new TreeMap<>(GAMES_MAP).forEach((number, game) -> System.out.printf("%s - %s\n", number, game));
-
-        System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
         try (Scanner scanner = new Scanner(System.in)) {
@@ -34,8 +29,12 @@ public final class App {
 
             switch (number) {
                 case 1 -> Cli.greeting();
+                case 2 -> EvenGame.run();
+                case 3 -> CalculatorGame.run();
+                case 4 -> GCDGame.run();
+                case 5 -> ProgressionGame.run();
+                case 6 -> PrimeGame.run();
                 case 0 -> System.exit(0);
-                default -> GAMES_MAP.get(number).run();
             }
         }
     }

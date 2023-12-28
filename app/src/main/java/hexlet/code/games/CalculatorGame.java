2d0c53651181b6c1cf76f09e011206aa0843d38d
zHd4 <computersystembase@gmail.com>
2023-12-28 19:13:50 +0100

    Update CalculatorGame

--- app/src/main/java/hexlet/code/games/CalculatorGame.java
+++ app/src/main/java/hexlet/code/games/CalculatorGame.java
@@ -1,27 +1,25 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public final class CalculatorGame {
     private static final String RULES = "What is the result of the expression?";
     private static final int MAX_NUMBER = 100;
 
     public static void run() {
-        Random random = new Random();
-
         List<Character> operators = List.of('+', '-', '*');
         List<Map<String, String>> rounds = new ArrayList<>();
 
         for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
-            int number1 = random.nextInt(MAX_NUMBER) + 1;
-            int number2 = random.nextInt(MAX_NUMBER) + 1;
+            int number1 = Utils.getRandomInt(1, MAX_NUMBER);
+            int number2 = Utils.getRandomInt(1, MAX_NUMBER);
 
-            Character operator = operators.get(random.nextInt(operators.size()));
+            Character operator = operators.get(Utils.getRandomInt(0, operators.size()));
 
             String question = String.format("%s %s %s", number1, operator, number2);
             int answer = calculate(number1, number2, operator);
@@ -33,18 +31,14 @@ public final class CalculatorGame {
     }
 
     private static int calculate(int number1, int number2, char operator) {
-        int result;
-
         if (operator == '+') {
-            result = number1 + number2;
+            return number1 + number2;
         } else if (operator == '-') {
-            result = number1 - number2;
+            return number1 - number2;
         } else if (operator == '*') {
-            result = number1 * number2;
+            return number1 * number2;
         } else {
             throw new RuntimeException("Invalid operator");
         }
-
-        return result;
     }
 }

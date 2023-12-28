f2dc8b24ab4ad9d964670e515a310d146859768f
zHd4 <computersystembase@gmail.com>
2023-12-28 19:01:08 +0100

    Update Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,12 +1,13 @@
 package hexlet.code;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
 public final class Engine {
     public static final int ROUNDS_COUNT = 3;
 
-    public static void start(Map<String, String> rounds, String rules) {
+    public static void start(List<Map<String, String>> rounds, String rules) {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Welcome to the Brain Games!");
             System.out.print("May I have your name? ");
@@ -16,9 +17,9 @@ public final class Engine {
             System.out.printf("Hello, %s!\n", username);
             System.out.println(rules);
 
-            for (Map.Entry<String, String> round : rounds.entrySet()) {
-                String question = round.getKey();
-                String correctAnswer = round.getValue();
+            for (Map<String, String> round : rounds) {
+                String question = round.get("question");
+                String correctAnswer = round.get("answer");
 
                 System.out.println("Question: " + question);
                 System.out.print("Your answer: ");

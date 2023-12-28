1a90609f8d69e8d2b9f56ff05a074e3a61d1fb22
zHd4 <computersystembase@gmail.com>
2023-12-28 19:13:42 +0100

    Update EvenGame

--- app/src/main/java/hexlet/code/games/EvenGame.java
+++ app/src/main/java/hexlet/code/games/EvenGame.java
@@ -1,22 +1,21 @@
 package hexlet.code.games;
 
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 public final class EvenGame {
     private static final String RULES = "Answer 'yes' if the number is even, otherwise answer 'no'.";
     private static final int MAX_NUMBER = 999;
 
     public static void run() {
-        Random random = new Random();
         List<Map<String, String>> rounds = new ArrayList<>();
 
         for (int i = 0; i < Engine.ROUNDS_COUNT; i++) {
-            int number = random.nextInt(MAX_NUMBER) + 1;
+            int number = Utils.getRandomInt(1, MAX_NUMBER);
 
             String question = String.valueOf(number);
             String answer = isEven(number) ? "yes" : "no";
